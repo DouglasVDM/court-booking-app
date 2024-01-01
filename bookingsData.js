8c98751196203b2fbf5258f67eae78bb03730b5a
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2024-01-02 01:05:52 +0200

    Added mock booking data

---
+++ bookingsData.js
@@ -0,0 +1,20 @@
+export const bookingsData = [
+  {
+    booking_id: 1,
+    court_id: 1,
+    member_id: 1,
+    booked_at: "2023-12-30T11:23:56.542Z",
+    booking_date: "2023-12-30T22:00:00.000Z",
+    booking_start_at: "12:00:00",
+    booking_end_at: "13:00:00",
+  },
+  {
+    booking_id: 2,
+    court_id: 8,
+    member_id: 2,
+    booked_at: "2023-12-30T14:34:43.327Z",
+    booking_date: "2023-01-30T22:00:00.000Z",
+    booking_start_at: "11:00:00",
+    booking_end_at: "17:00:00",
+  },
+];
