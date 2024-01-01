601fac69f29af2cb012fd2d9f49bfc9e5003436d
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2024-01-02 01:12:52 +0200

    Updated component import reference

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import { useState } from "react";
-import Court from "./components/Court";
+import Courts from "./components/Courts";
 import { bookingsData } from "../bookingsData";
 import { courtsData } from "./courtsData";
 
 function App() {
   const [courts, setCourts] = useState(courtsData);
   const [bookings, setBookings] = useState(bookingsData);
-  console.log('bookingsData:',bookingsData)
-  console.log('courtsData:',courtsData)
+  console.log("bookingsData:", bookingsData);
+  console.log("courtsData:", courtsData);
 
   return (
     <>
       <h1>Court Booking App</h1>
-      <Court courts={courts} />
+      <Courts courts={courts} />
     </>
   );
 }
