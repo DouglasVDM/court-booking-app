b59569f145ba2cdd656a078354791c9dc3b38b82
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2024-01-03 22:35:10 +0200

    Fetch bookings custom hook

---
+++ src/customHooks/useBookings.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const useBookings = (apiEndpointPrefix) => {
+  const [bookings, setBookings] = useState([]);
+
+  useEffect(() => {
+    const fetchBookings = async () => {
+      try {
+        const bookingsResponse = await axios.get(`${apiEndpointPrefix}/bookings`);
+        setBookings(bookingsResponse.data);
+        console.log("bookingsResponse", bookingsResponse.data);
+      } catch (err) {
+        console.error("Error fetching bookings:", err.message);
+      }
+    };
+
+    fetchBookings();
+  }, [apiEndpointPrefix]);
+
+  return { bookings };
+};
+
+export default useBookings;
