ff3d0b45c1aa5057b0fd96a1e3c8e22fe74e810f
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2024-01-03 23:10:41 +0200

    Imported components and custom hooks for fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,64 @@
 import { useState } from "react";
-import Courts from "./components/Courts";
-import { bookingsData } from "../bookingsData";
-import { courtsData } from "./courtsData";
+import Button from "react-bootstrap/Button";
 
-function App() {
-  const [courts, setCourts] = useState(courtsData);
-  const [bookings, setBookings] = useState(bookingsData);
-  console.log("bookingsData:", bookingsData);
-  console.log("courtsData:", courtsData);
+// Components
+import Courts from "./components/Courts";
+import BookingForm from "./components/BookingForm";
+import DaysOfWeek from "./components/DaysOfWeek";
+import Durations from "./components/Durations";
+import StartTimes from "./components/StartTimes";
+import BookingType from "./components/BookingType";
+
+// Custom Hooks for fetching data
+import useBookingTypes from "./customHooks/useBookingTypes";
+import useDurations from "./customHooks/useDurations";
+import useStartTimes from "./customHooks/useStartTimes";
+import useDaysOfWeek from "./customHooks/useDaysOfWeek";
+import useBookings from "./customHooks/useBookings";
+import useCourts from "./customHooks/userCourts";
+
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [show, setShow] = useState(true);
+
+  const apiEndpointPrefix = import.meta.env.VITE_API_ENDPOINT;
+
+  const { bookingTypes } = useBookingTypes(apiEndpointPrefix);
+  const { durations } = useDurations(apiEndpointPrefix);
+  const { startTimes } = useStartTimes(apiEndpointPrefix);
+  const { daysOfWeek } = useDaysOfWeek(apiEndpointPrefix);
+  const { bookings } = useBookings(apiEndpointPrefix);
+  const { courts } = useCourts(apiEndpointPrefix);
+
+  const handleClick = () => {
+    setShow(!show);
+  };
 
   return (
     <>
       <h1>Court Booking App</h1>
+      <br />
+      <Button onClick={handleClick} variant="primary" size="lg">
+        Book a court
+      </Button>
+      <br />
+      <br />
+      {show ? "" : <BookingForm courts={courts} bookings={bookings} />}
       <Courts courts={courts} />
+      <br />
+      <br />
+      <DaysOfWeek daysOfWeek={daysOfWeek} />
+      <br />
+      <br />
+      <StartTimes startTimes={startTimes} />
+      <br />
+      <br />
+      <Durations durations={durations} />
+      <br />
+      <br />
+      <BookingType bookingTypes={bookingTypes} />
     </>
   );
-}
+};
 
 export default App;
