5cdffe10c2f003426a4a042c14822657ff1d9b9d
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2024-01-03 21:39:19 +0200

    Sample data

---
+++ src/data/courtsData.js
@@ -0,0 +1,18 @@
+export const courtsData = [
+  {
+    court_id: 1,
+    court_name: "Court 1",
+  },
+  {
+    court_id: 6,
+    court_name: "Court 2",
+  },
+  {
+    court_id: 7,
+    court_name: "Court 3",
+  },
+  {
+    court_id: 8,
+    court_name: "Court 4",
+  },
+];
---
+++ src/data/durationsData.js
@@ -0,0 +1,14 @@
+export const durationsData = [
+  {
+    duration_id: 1,
+    duration_hours: 1,
+  },
+  {
+    duration_id: 2,
+    duration_hours: 2,
+  },
+  {
+    duration_id: 3,
+    duration_hours: 3,
+  },
+];
