95563298390c016ed4689aa7eb23116d16ec7b2c
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2024-01-06 09:15:34 +0200

    Passed selected item state as prop down to child

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
-import Form from "react-bootstrap/Form";
+import { useForm } from "react-hook-form";
+import { Form, Button } from "react-bootstrap";
+import axios from "axios";
+
 import BookingType from "./BookingType";
 import Courts from "./Courts";
 import DaysOfWeek from "./DaysOfWeek";
@@ -15,6 +18,8 @@ const BookingForm = ({
   bookingTypes,
   members,
 }) => {
+  const { register, handleSubmit } = useForm();
+
   const [selectedCourt, setSelectedCourt] = useState(null);
   const [selectedDayOfWeek, setSelectedDayOfWeek] = useState(null);
   const [selectedStartTime, setSelectedStartTime] = useState(null);
@@ -41,37 +46,67 @@ const BookingForm = ({
     setSelectedMember(member);
   };
 
+  // const onSubmit = async (event) => {
+  //   event.preventDefault()
+  //   const bookingFormData = {
+  //     court_id: event.target.value,
+  //     member_id: selectedMember ? selectedMember.member_id : null,
+  //     booked_at: new Date().toISOString(),
+  //     day_name: selectedDayOfWeek ? selectedDayOfWeek.day_name : null,
+  //     start_time: selectedStartTime ? selectedStartTime.start_time : null,
+  //     duration_hours: selectedDuration ? selectedDuration.duration_hours : null,
+  //     booking_type_name: selectedBookingType
+  //       ? selectedBookingType.booking_type_name
+  //       : null,
+  //   };
+
+  //   try {
+  //     const bookingResponse = await axios.post("/", bookingFormData);
+  //     console.log("Booking created:", bookingResponse.data);
+  //   } catch (err) {
+  //     console.error("Error creating booking", err.message);
+  //   }
+  // };
+
   return (
     <>
-      <Members members={members} onMemberSelected={handleMemberSelected} />
+      {!selectedMember ? (
+        <Members members={members} onMemberSelected={handleMemberSelected} />
+      ) : (
+        selectedMember.first_name
+      )}
       <br />
       <br />
-      <Courts courts={courts} onCourtSelected={handleCourtSelected} />
+        <Courts courts={courts} onCourtSelected={handleCourtSelected} />
       <br />
       <br />
       <DaysOfWeek
         daysOfWeek={daysOfWeek}
+        selectedDayOfWeek={selectedDayOfWeek}
         onDayOfWeekSelected={handleDayOfWeekSelected}
       />
       <br />
       <br />
       <StartTimes
         startTimes={startTimes}
+        selectedStartTime={selectedStartTime}
         onStartTimeSelected={handleStartTimeSelected}
       />
       <br />
       <br />
       <Durations
         durations={durations}
+        selectedDuration={selectedDuration}
         onDurationSelected={handleDurationSelected}
       />
       <br />
       <br />
       <BookingType
         bookingTypes={bookingTypes}
+        selectedBookingType={selectedBookingType}
+        setBookingType={setSelectedBookingType}
         onBookingTypeSelected={handleBookingTypeSelected}
       />
-
       <br />
     </>
   );
