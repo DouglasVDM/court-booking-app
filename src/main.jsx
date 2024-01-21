001752fdc43e43bc42fb1e052770288be4fd0ed1
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2024-01-21 11:00:52 +0200

    Configure the Auth0Provider component

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
 import App from "./App.jsx";
 
 const root = createRoot(document.getElementById("root"));
 const domain = import.meta.env.DOMAIN;
-const clientId = import.meta.env.CLIEN_ID;
+const clientId = import.meta.env.CLIENT_ID;
 
 root.render(
   <Auth0Provider
@@ -16,6 +15,8 @@ root.render(
       redirect_uri: window.location.origin,
     }}
   >
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Auth0Provider>
 );
