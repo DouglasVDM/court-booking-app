632bc83d1dddf042790517fb8baa59cd5a410728
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2024-02-24 16:40:44 +0200

    Message service

---
+++ src/services/message-service.js
@@ -0,0 +1,56 @@
+import { callExternalApi } from "./external-api.service";
+
+const apiServerUrl = import.meta.env.VITE_API_ENDPOINT;
+console.log("apiServerUrl",apiServerUrl)
+
+
+export const getPublicResource = async () => {
+  const config = {
+    url: `${apiServerUrl}/`,
+    method: "GET",
+    headers: {
+      "content-type": "application/json",
+    },
+  };
+
+  const { data, error } = await callExternalApi({ config });
+
+  return {
+    data: data || null,
+    error,
+  };
+};
+
+export const getProtectedResource = async () => {
+  const config = {
+    url: `${apiServerUrl}/`,
+    method: "GET",
+    headers: {
+      "content-type": "application/json",
+    },
+  };
+
+  const { data, error } = await callExternalApi({ config });
+
+  return {
+    data: data || null,
+    error,
+  };
+};
+
+export const getAdminResource = async () => {
+  const config = {
+    url: `${apiServerUrl}/`,
+    method: "GET",
+    headers: {
+      "content-type": "application/json",
+    },
+  };
+
+  const { data, error } = await callExternalApi({ config });
+
+  return {
+    data: data || null,
+    error,
+  };
+};
