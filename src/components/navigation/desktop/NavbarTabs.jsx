99b3b71ffc2e8331b30739b2c42d72941f6d7bce
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2024-03-02 19:38:19 +0200

    Use semantic header tag

--- src/components/navigation/desktop/NavbarTabs.jsx
+++ src/components/navigation/desktop/NavbarTabs.jsx
@@ -6,13 +6,13 @@ const NavbarTabs = () => {
 
   return (
     <div className="m-4">
-      <NavbarTab path="/profile" label="Profile" />
-      <br />
-      <NavbarTab path="/public" label="Public" />
+      <NavbarTab path="/about" label="About" />
       <br />
       <br />
       {isAuthenticated && (
-        <>
+        <header>
+          <NavbarTab path="/profile" label="Profile" />
+          <br />
           <NavbarTab path="/bookings" label="All bookings" />
           <br />
           <NavbarTab path="/booking-form" label="Book a court" />
@@ -20,7 +20,7 @@ const NavbarTabs = () => {
           <NavbarTab path="/protected" label="Protected" />
           <br />
           <NavbarTab path="/admin" label="Admin" />
-        </>
+        </header>
       )}
     </div>
   );
