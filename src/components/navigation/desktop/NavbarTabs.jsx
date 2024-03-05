a5824772217e6b558ca81d18aaee10fc810e421a
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2024-03-05 22:17:14 +0200

    Add route back to  Home on every page

--- src/components/navigation/desktop/NavbarTabs.jsx
+++ src/components/navigation/desktop/NavbarTabs.jsx
@@ -5,12 +5,33 @@ const NavbarTabs = () => {
   const { isAuthenticated } = useAuth0();
 
   return (
-    <div className="m-4">      
+    <div className="m-4">
       {isAuthenticated && (
         <header>
-          <NavbarTab path="/bookings" label="All bookings" />
           <br />
-          <NavbarTab path="/booking-form" label="Book a court" />
+          {window.location.pathname === "/" ? (
+            <>
+              <NavbarTab path="/bookings" label="All bookings" />
+              <br />
+              <NavbarTab path="/booking-form" label="Book a court" />
+            </>
+          ) : (
+            <>
+              {window.location.pathname === "/booking-form" ? (
+                <>
+                  <NavbarTab path="/" label="Home" />
+                  <br />
+                  <NavbarTab path="/bookings" label="All bookings" />
+                </>
+              ) : (
+                <>
+                  <NavbarTab path="/" label="Home" />
+                  <br />
+                  <NavbarTab path="/booking-form" label="Book a court" />
+                </>
+              )}
+            </>
+          )}
         </header>
       )}
     </div>
