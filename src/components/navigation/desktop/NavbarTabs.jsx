a62f2f74262a2418209e2e60b4dceb9dfc35d0fc
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2024-03-05 20:24:10 +0200

    Remove unused routes

--- src/components/navigation/desktop/NavbarTabs.jsx
+++ src/components/navigation/desktop/NavbarTabs.jsx
@@ -5,21 +5,12 @@ const NavbarTabs = () => {
   const { isAuthenticated } = useAuth0();
 
   return (
-    <div className="m-4">
-      <NavbarTab path="/about" label="About" />
-      <br />
-      <br />
+    <div className="m-4">      
       {isAuthenticated && (
         <header>
-          <NavbarTab path="/profile" label="Profile" />
-          <br />
           <NavbarTab path="/bookings" label="All bookings" />
           <br />
           <NavbarTab path="/booking-form" label="Book a court" />
-          <br />
-          <NavbarTab path="/protected" label="Protected" />
-          <br />
-          <NavbarTab path="/admin" label="Admin" />
         </header>
       )}
     </div>
