6d89403dbb1ec5f426b8e796633bf2601a4316fb
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2024-03-15 22:16:00 +0200

    Hero image

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,5 +1,30 @@
+import { Container, Row, Col } from "react-bootstrap";
+
+// Import your hero image
+import heroImage from "../assets/mayfield-tennis-club.jpg";
+
 const HeroBanner = () => {
-  return <div className="m-4">HeroBanner</div>;
+  return (
+    <div>
+      {/* Hero image */}
+      <Container fluid className="p-0">
+        <Row
+          className="justify-content-center align-items-center"
+          style={{
+            minHeight: "100vh",
+            backgroundImage: `url(${heroImage})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          <Col className="text-center text-light">
+            <h1>Welcome to My App</h1>
+            <p>Explore and book your favorite courts!</p>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default HeroBanner;
